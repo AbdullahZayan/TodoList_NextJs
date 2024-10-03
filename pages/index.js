d27934800e81e1124e912172991e266e5e1c8d4f
Abdullah <abdullahzayan.my@gmail.com>
2024-10-03 12:17:46 +0800

    Initial commit with website files

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,139 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import { format } from "date-fns";
+import { useState, StrictMode } from "react";  // Import StrictMode here
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todo, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 999);
+  };
+
+  const handleKeyUp = (key) => {
+    if (key === "Enter" && newTodo) {
+      const randomNumber = getRandomNumber();
+      const newItem = {
+        id: `item-${randomNumber}`,
+        content: newTodo,
+      };
+
+      setTodo(todo.concat(newItem));
+      setNewTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    setTodo(todo.filter((_, index) => index !== id));
+  };
+
+  const render = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const handleDnDragEnd = (result) => {
+    const { destination, source } = result;
+
+    if (!destination) return;
+
+    const items = render(todo, source.index, destination.index);
+    setTodo(items);
+  };
+
+  return (
+    <div className="flex justify-center pt-40">
+      <div className="max-w-sm w-full shadow-lg bg-white p-8 rounded-xl opacity-70">
+        <div className="flex justify-center cursor-default bg-gray-200 rounded-3xl px-4 py-1 color-gray hover:scale-110 transition-all">
+          <img
+            className="object-cover rounded-full w-16 h-16 n-2"
+            src="https://avatars.githubusercontent.com/u/111651863?v=4"
+            alt="Zayan"
+          ></img>
+          <div className="w-full p-3">
+            <p className="text-3xl text-white-600">ToDo List</p>
+            <p className="text-sm">{format(new Date(), "MMM d, yyyy")}</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="relative mt-10">
+          <div className="absolute inset-y-0 left-2 flex items-center pl-3 pointer-events-none">
+            <svg
+              xmlns="https://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi"
+            >
+              <path d="M8 4a.5.5 0 0 1 .5.5V7.5h3a.5.5 0 0 1 0 1h-3V11.5a.5.5 0 0 1-1 0V8.5H4a.5.5 0 0 1 0-1h3V4.5A.5.5 0 0 1 8 4z" />
+            </svg>
+          </div>
+
+          <input
+            type="text"
+            id="newToDo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            onKeyUp={(e) => handleKeyUp(e.key)}
+            className="block w-full pl-10 p-2 border-4 rounded-full bg-gray-600 text-white"
+            placeholder="new todo item"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <StrictMode>  {/* Wrap DragDropContext with StrictMode */}
+          <DragDropContext onDragEnd={handleDnDragEnd}>
+            <Droppable droppableId="droppable">
+              {(droppableProvided) => (
+                <div
+                  {...droppableProvided.droppableProps}
+                  ref={droppableProvided.innerRef}
+                >
+                  <ul className="block w-full pt-6">
+                    {todo?.map((item, index) => (
+                      <Draggable
+                        draggableId={item.id}
+                        key={item.id}
+                        index={index}
+                      >
+                        {(draggableProvided) => (
+                          <li
+                            {...draggableProvided.draggableProps}
+                            {...draggableProvided.dragHandleProps}
+                            ref={draggableProvided.innerRef}
+                            className="w-full border-2 rounded-xl mt-2 hover:border-blue-300"
+                          >
+                            <input
+                              id={index}
+                              type="checkbox"
+                              className="float-left block w-6 h-6 m-3"
+                            />
+                            <button
+                              id={index}
+                              onClick={() => handleDelete(index)}
+                              className="float-right w-7 h-7 m-2.5 rounded-2xl bg-red-700 text-gray-200 shadow-md hover:bg-red-500 hover:scale-105"
+                            >
+                              x
+                            </button>
+                            <label htmlFor={index} className="block w-full p-3">
+                              {item.content}
+                            </label>
+                          </li>
+                        )}
+                      </Draggable>
+                    ))}
+                  </ul>
+                  {droppableProvided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </StrictMode>
+      </div>
     </div>
   );
 }
